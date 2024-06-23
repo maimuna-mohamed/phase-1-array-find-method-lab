bd2867be46bc586ee76705be1eab9fd9783eccbf
maimuna-mohamed <maimunamohamedaden17@gmail.com>
2024-06-23 23:55:23 +0300

    complete project

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+// function superbowlWin(record) {
+//     const win = record.find(entry => entry.result === "w")
+//     return win ? win.year : undefined;
+// }
+ 
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+    return win ? win.year : undefined;
+  }
